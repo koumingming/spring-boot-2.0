cd201448705894094b677f292b8fa65ca74e0b32
koumingming <koumingming@aliyun.com>
2018-11-06 11:52:13 +0800

    add swagger2

--- business-service/web-service/src/main/java/com/boot/controller/HelloWorld.java
+++ business-service/web-service/src/main/java/com/boot/controller/HelloWorld.java
@@ -1,6 +1,10 @@
 package com.boot.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,10 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @Description:
  */
 @RestController
+@Api(tags = "测试接口-API")
 public class HelloWorld {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello World" + System.currentTimeMillis();
+    @ApiOperation("hello的测试接口")
+    @ApiImplicitParam(name = "name", value = "名称", required = true, dataType = "String")
+    public String hello(@RequestParam(required = true) final String name) {
+        return name + "Hello World" + System.currentTimeMillis();
     }
 }
